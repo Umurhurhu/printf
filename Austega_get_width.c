b4e004467fb97e20d2c8060daf0bc37ab53b553a
AUSTINE BRIAN <132396275+Austine33@users.noreply.github.com>
2023-07-26 08:58:31 +0300

    Update Austega_get_width.c

--- Austega_get_width.c
+++ Austega_get_width.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
- * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
+ * get_width - Calculates width for printing
+ * @format: Formats string in which to print the arguments.
+ * @i: List arguments to be printed.
  * @list: list of arguments.
  * Return: width.
  */

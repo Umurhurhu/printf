6a84996b5a1307a6efa7bbe9e56f09ceb2014c32
AUSTINE BRIAN <132396275+Austine33@users.noreply.github.com>
2023-07-26 09:04:51 +0300

    Update Austega_handle_print.c

--- Austega_handle_print.c
+++ Austega_handle_print.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
+ * handle_print - Prints arguments based on type
+ * @fmt: Formats string in which to print the arguments.
+ * @list: List arguments to be printed.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
+ * @flags: Calculating active flags
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
